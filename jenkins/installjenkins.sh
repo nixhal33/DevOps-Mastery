f0629cdc075fcc78ab7bea7745f33442e4b2e287
nixhal33 <nischal.koirala33@gmail.com>
2026-04-08 11:32:04 +0545

    Install jenkins safely using this script

---
+++ jenkins/installjenkins.sh
@@ -0,0 +1,29 @@
+# First install java always before installing jenkins
+sudo apt update -y
+
+sudo apt install fontconfig openjdk-21-jre
+
+java -version
+
+sudo mkdir -p /etc/apt/keyrings
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y
+
+sudo apt install jenkins
+
+#allow firewalls for jenkins
+
+sudo ufw allow 8080
+
+sudo ufw allow OpenSSH
+
+sudo ufw enable
+
+sudo ufw status
+
+# To display the jenkins password
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
